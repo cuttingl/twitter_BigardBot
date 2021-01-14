f73eb0188f8d9424f608ea46d1daefe65555c6fa
laurent <laurent.cutting@gmail.com>
2021-01-14 15:49:18 +0100

    deleting reading in files auth keys

--- twitterbot.py
+++ twitterbot.py
@@ -3,13 +3,6 @@ import datetime
 import os
 import credentials
 
-
-def readTokens(tokens):
-    with open("./tokens_bigardbot.txt", 'r') as file:
-        for line in file:
-            for word in line.split():
-                tokens.append(word)
-
 def publishTweetFromInput(api):
     text = input("le text de votre tweet : ")
     api.update_status(text)
@@ -20,9 +13,6 @@ def senddmtest(api):
     api.send_direct_message('hugobollon', "t une énaurme merde")
 
 if __name__ == "__main__":
-    tokens = []
-    readTokens(tokens)
-    print(tokens)
     auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
     auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
     api = tweepy.API(auth)
