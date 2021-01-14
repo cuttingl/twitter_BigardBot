454930dfdc7ea4deeb9ce992af9bfc047438375d
laurent <laurent.cutting@gmail.com>
2021-01-14 18:33:46 +0100

    jawsdb connection added // pipfile added // requirements added for mysql

--- twitterbot.py
+++ twitterbot.py
@@ -1,7 +1,7 @@
 import tweepy
 from datetime import datetime
 import os
-import credentials
+#import credentials
 
 def publishTweetFromInput(api):
     text = input("le text de votre tweet : ")
@@ -14,13 +14,24 @@ def senddmtest(api):
     api.send_direct_message('906078704', "test is doing fine")
     api.send_direct_message('945425370123251713', "salut hehe ceci est un test, envoyé à ... " + current_time)
 
-if __name__ == "__main__":
-    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
-    auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
-    api = tweepy.API(auth)
+def botRoutine(api):
 
-    senddmtest(api)
-    
+    now = datetime.now().time()
+    current_time = now.strftime("%H:%M:%S")
+    while (True):
+        if current_time == "19:00":
+            api.update_status("""random_text_generated""")
+
+
+if __name__ == "__main__":
+    """auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+    auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
+    api = tweepy.API(auth)"""
+
+    ##senddmtest(api)
+    now = datetime.now().time()
+    current_time = now.strftime("%H:%M")
+    print(current_time == "18:00")
     ##publishTweetFromInput(api)
 
 
