b401ac0c99035ffe5964ae8b05d2832342114573
laurent <laurent.cutting@gmail.com>
2021-01-14 13:55:58 +0100

    new test to send dm

--- twitterbot.py
+++ twitterbot.py
@@ -17,6 +17,7 @@ def publishTweetFromInput(api):
 
 def senddmtest(api):
     api.send_direct_message('LaurentCutting', "test")
+    api.send_direct_message('hugobollon', "t une énaurme merde")
 
 if __name__ == "__main__":
     tokens = []
