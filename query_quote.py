a74f89ec6a5bdc5920943722da7ef532127cd28a
laurent <laurent.cutting@gmail.com>
2021-01-15 21:11:42 +0100

    method outside of twitterbot

--- query_quote.py
+++ query_quote.py
@@ -4,30 +4,25 @@ import random
 
 
 def query_quote():
+    numrandom = 0
+    quote = ""
     with connectDB.connection:
          with connectDB.connection.cursor() as cursor:
-            print(random_number())
-            sql = "SELECT `citation` FROM bigardTwitterBot WHERE `id`="+ str(random_number())
-            cursor.execute(sql)
-            resultQuote = cursor.fetchone()
-            cursor.close()
-            connectDB.connection.close()
 
-            quote = resultQuote[0]            
-            return quote
-
-
-def random_number():
-     with connectDB.connection:
-         with connectDB.connection.cursor() as cursor:
             sql1 = "SELECT COUNT(*) FROM bigardTwitterBot"
             cursor.execute(sql1)
-            resultNum = cursor.fetchone()
-            cursor.close()
-            connectDB.connection.close()
 
+            resultNum = cursor.fetchone()
             number_cols = resultNum[0]
             numrandom = random.randint(0, number_cols-1)
-            return numrandom
+
+            sql = "SELECT `citation` FROM bigardTwitterBot WHERE `id`="+ str(numrandom)
+            cursor.execute(sql)
+            
+            resultQuote = cursor.fetchone()
+            quote = resultQuote[0]
+            cursor.close()
+            connectDB.connection.close()
+    return quote
     
 
--- twitterbot.py
+++ twitterbot.py
@@ -13,32 +13,9 @@ def publishTweetFromInput(api):
     print(text)
     print(text + " salut ceci est un test")
 
-def senddmtest(ap, connection):
+def senddmtest(api):
 
-    numrandom = 0
-    quote = ""
-
-    with connection:
-         with connectDB.connection.cursor() as cursor:
-
-            sql1 = "SELECT COUNT(*) FROM bigardTwitterBot"
-            cursor.execute(sql1)
-
-            resultNum = cursor.fetchone()
-            number_cols = resultNum[0]
-            numrandom = random.randint(0, number_cols-1)
-
-            sql = "SELECT `citation` FROM bigardTwitterBot WHERE `id`="+ str(numrandom)
-            cursor.execute(sql)
-            
-            resultQuote = cursor.fetchone()
-            quote = resultQuote[0]
-            cursor.close()
-            connectDB.connection.close()
-
-    api.send_direct_message('906078704', quote)
-    api.send_direct_message('906078704', "salut")
-    
+    api.send_direct_message('906078704', query_quote.query_quote())
 
 def botRoutine(api):
 
@@ -53,12 +30,8 @@ if __name__ == "__main__":
     auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
     auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
     api = tweepy.API(auth)
-    connection = pymysql.connect(host=credentials.JaHost,
-                             user=credentials.JaUser,
-                             password=credentials.JaPwrd,
-                             database=credentials.JaName)
 
-    senddmtest(api, connection)
+    senddmtest(api)
     now = datetime.now().time()
     current_time = now.strftime("%H:%M")
     print(current_time == "18:00")
