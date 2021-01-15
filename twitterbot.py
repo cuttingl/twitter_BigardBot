4b754ff9e6ab52419cdf3041135d3a1c15a24759
laurent <laurent.cutting@gmail.com>
2021-01-15 21:54:52 +0100

    error statement closed

--- twitterbot.py
+++ twitterbot.py
@@ -13,13 +13,13 @@ def publishTweetFromInput(api):
     print(text)
     print(text + " salut ceci est un test")
 
-def senddmtest(api):
+def senddmtest(api, connection):
 
     numrandom = 0
     quote = ""
 
-    with connectDB.connection:
-         with connectDB.connection.cursor() as cursor:
+    with connection:
+         with connection.cursor() as cursor:
 
             sql1 = "SELECT COUNT(*) FROM bigardTwitterBot"
             cursor.execute(sql1)
@@ -34,11 +34,11 @@ def senddmtest(api):
             resultQuote = cursor.fetchone()
             quote = resultQuote[0]
             cursor.close()
-            connectDB.connection.close()
+            connection.close()
 
     api.send_direct_message('906078704', quote)
 
-def botRoutine(api):
+def botRoutine(api, connection):
 
     now = datetime.now().time()
     current_time = now.strftime("%H:%M:%S")
@@ -75,10 +75,14 @@ if __name__ == "__main__":
     auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
     auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
     api = tweepy.API(auth)
+    connection = pymysql.connect(host=credentials.JaHost,
+                             user=credentials.JaUser,
+                             password=credentials.JaPwrd,
+                             database=credentials.JaName)
 
-    senddmtest(api)
+    senddmtest(api, connection)
 
-    ##botRoutine(api)
+    ##botRoutine(api, connection)
 
     now = datetime.now().time()
     current_time = now.strftime("%H:%M")
